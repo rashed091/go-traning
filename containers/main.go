5ac03c0f99484d43479f93e05e296ed1ff6460a5
mrasheduzzaman <rashed091nsu@gmail.com>
2022-05-07 15:08:51 +0600

    add list container

--- containers/main.go
+++ containers/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"container/list"
 	"fmt"
-	"sort"
 )
 
 func main() {
@@ -11,8 +10,6 @@ func main() {
 	x.PushBack(5)
 	x.PushBack(2)
 	x.PushBack(3)
-
-	sort.Sort(x)
 
 	for e := x.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value.(int))
